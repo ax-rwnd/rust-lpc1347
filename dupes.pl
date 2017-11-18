e353ddfcc5f17f834d5fe58ecce4c30cd5ca147b
Axel <axel-sundbom@hotmail.com>
2017-11-18 17:03:45 +0100

    Adds script to find duplicate SVD entries.

---
+++ dupes.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+# Create a map for the names
+my %names;
+
+# Parse names from file
+if (open (F, "$ARGV[0]")) {
+    while (read(F, $buf, 4096) > 0) {
+        @matches = $buf =~ m/<name>(\w+)/g;
+        foreach my $el (@matches) {
+            if (!exists $names{$el}) {
+                $names{$el} = 1;
+            } else {
+                $names{$el}++;
+            }
+        }
+    }
+}
+
+foreach my $el (keys %names) {
+    if ($names{$el} > 1) {
+        print("$el: $names{$el}\n");
+    }
+}
